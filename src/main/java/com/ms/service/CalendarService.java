0d054e581a317731fa9cdcf65539b7f6b306ca4b
SANAYEE <dudtkd0219@gmail.com>
2023-09-04 23:09:40 +0900

    fundamental calendar service

---
+++ src/main/java/com/ms/service/CalendarService.java
@@ -0,0 +1,26 @@
+package com.ms.service;
+
+import com.ms.entity.Calendar;
+import com.ms.repository.CalendarRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CalendarService {
+
+    private final CalendarRepository calendarRepository;
+
+    public void save(Calendar calendar) throws RuntimeException{
+        if(calendarRepository.existsByMemberAndYearAndMonthAndDay(calendar.getMember(), calendar.getYear(), calendar.getMonth(), calendar.getDay())){
+            throw new RuntimeException("already exists day please do not save. Do update !");
+        }
+        calendarRepository.save(calendar);
+    }
+
+    public Calendar findCalendar(Long id) throws RuntimeException{
+        return calendarRepository.findById(id).orElseThrow(()-> new RuntimeException(id+"is not exists in table"));
+    }
+}
