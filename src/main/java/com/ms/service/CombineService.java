3061477ace76eb0718e1b3c06fdd71e4ced59bef
SANAYEE1996 <dudtkd0219@gmail.com>
2023-09-21 12:44:00 +0900

    설계를 다시 해야합니당 !!!

--- src/main/java/com/ms/service/CombineService.java
+++ src/main/java/com/ms/service/CombineService.java
@@ -2,24 +2,17 @@ package com.ms.service;
 
 import com.ms.dto.ScheduleDto;
 import com.ms.dto.ScheduleRequestDto;
-import com.ms.entity.Calendar;
-import com.ms.entity.Schedule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class CombineService {
-
-    private final CalendarService calendarService;
-
-    private final ScheduleService scheduleService;
-
-    private final ScheduleFindService scheduleFindService;
 
     private final ValidationCheck validationCheck;
 
@@ -28,16 +21,15 @@ public class CombineService {
     public void saveSchedule(ScheduleDto scheduleDto) throws RuntimeException{
         validationCheck.checkDate(scheduleDto);
         validationCheck.checkTime(scheduleDto);
-        Long calendarId = (scheduleDto.getCalendarId() != null) ? scheduleDto.getCalendarId() : calendarService.save(converter.toCalendar(scheduleDto));
-        Calendar calendar = calendarService.findCalendar(calendarId);
-        scheduleService.save(new Schedule(0L, calendar, scheduleDto.getStartHour(), scheduleDto.getStartMin(), scheduleDto.getEndHour(), scheduleDto.getEndMin(), scheduleDto.getTitle(), scheduleDto.getNote()));
+
     }
 
     public List<ScheduleDto> findScheduleForMonth(ScheduleRequestDto scheduleRequestDto) throws RuntimeException{
-        return scheduleFindService.findScheduleForMonth(scheduleRequestDto);
+
+        return new ArrayList<>();
     }
 
     public void deleteSchedule(Long schedule_id){
-        scheduleService.delete(schedule_id);
+
     }
 }
