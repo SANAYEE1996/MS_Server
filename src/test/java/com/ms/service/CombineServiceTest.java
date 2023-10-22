5e4cda67b755e37dedbb231a83a75db3f97f8596
SANAYEE <dudtkd0219@gmail.com>
2023-10-22 21:16:03 +0900

    update repository 수정, test passed

--- src/test/java/com/ms/service/CombineServiceTest.java
+++ src/test/java/com/ms/service/CombineServiceTest.java
@@ -3,12 +3,15 @@ package com.ms.service;
 import com.ms.dto.NotificationDto;
 import com.ms.dto.ScheduleDto;
 import com.ms.dto.ScheduleRequestDto;
+import com.ms.entity.NotificationType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 public class CombineServiceTest {
@@ -74,5 +77,39 @@ public class CombineServiceTest {
         Long schedule_id = 4L;
 
         combineService.deleteSchedule(schedule_id);
+    }
+
+    @Test
+    void updateTest(){
+        List<NotificationDto> notificationDtoList = new ArrayList<>();
+        notificationDtoList.add(NotificationDto.builder().scheduleId(2L).type(NotificationType.MIN).value(3).notificationTime("2023-10-25 18:57").build());
+        notificationDtoList.add(NotificationDto.builder().scheduleId(2L).type(NotificationType.HOUR).value(1).notificationTime("2023-10-25 18:00").build());
+        ScheduleDto scheduleDto = ScheduleDto
+                .builder()
+                .scheduleId(2L)
+                .colorId(1L)
+                .startYear(2023)
+                .startMonth(10)
+                .startDay(25)
+                .startHour(19)
+                .startMin(00)
+                .endYear(2023)
+                .endMonth(10)
+                .endDay(25)
+                .endHour(21)
+                .endMin(10)
+                .title("세미나")
+                .note("자바 세미나")
+                .notificationDtoList(notificationDtoList)
+                .build();
+
+        combineService.updateSchedule(scheduleDto);
+
+        ScheduleRequestDto scheduleRequestDto = ScheduleRequestDto.builder().scheduleId(2L).build();
+
+        ScheduleDto respectSchedule = combineService.findScheduleForDay(scheduleRequestDto);
+
+        assertThat(scheduleDto.getTitle()).isEqualTo(respectSchedule.getTitle());
+        assertThat(scheduleDto.getNote()).isEqualTo(respectSchedule.getNote());
     }
 }
