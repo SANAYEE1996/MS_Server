8b9ec9887f8403a1c1fae3f7f7709f1d7975b246
SANAYEE1996 <dudtkd0219@gmail.com>
2023-12-07 17:22:01 +0900

    member email 을 얻기 위한 webclient 사용

--- src/main/java/com/ms/reactive/ScheduleRouter.java
+++ src/main/java/com/ms/reactive/ScheduleRouter.java
@@ -1,5 +1,6 @@
 package com.ms.reactive;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -9,16 +10,20 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Component("scheduleRouter")
+@RequiredArgsConstructor
 public class ScheduleRouter {
+
+    private final JwtFilter jwtFilter;
 
     @Bean
     public RouterFunction<?> routeProduct(ScheduleHandler handler){
         return route()
                 .POST("/schedule/day", accept(MediaType.APPLICATION_JSON), handler::getDaySchedule)
                 .POST("/schedule/month", accept(MediaType.APPLICATION_JSON), handler::getMonthSchedule)
-                .POST("/schedule/save", accept(MediaType.APPLICATION_JSON), handler::saveSchedule)
-                .POST("/schedule/delete", accept(MediaType.APPLICATION_JSON), handler::deleteSchedule)
-                .POST("/schedule/update", accept(MediaType.APPLICATION_JSON), handler::updateSchedule)
+                .POST("/schedule/save", accept(MediaType.APPLICATION_JSON), handler::saveSchedule).filter(jwtFilter)
+                .POST("/schedule/delete", accept(MediaType.APPLICATION_JSON), handler::deleteSchedule).filter(jwtFilter)
+                .POST("/schedule/update", accept(MediaType.APPLICATION_JSON), handler::updateSchedule).filter(jwtFilter)
+                .GET("/schedule/test", handler::check).filter(jwtFilter)
                 .build();
     }
 }
